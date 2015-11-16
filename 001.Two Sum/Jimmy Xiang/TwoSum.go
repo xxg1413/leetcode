ac7f334d2cdafd3f98d6ea945f5d007c7c4636ea
Jimmy Xiang <xxg1413@gmail.com>
2015-11-16 17:22:55 +0800

    change the problem 1

--- 001.Two Sum/Jimmy Xiang/TwoSum.go
+++ 001.Two Sum/Jimmy Xiang/TwoSum.go
@@ -6,7 +6,7 @@ import (
 
 func twoSum(arr []int, value int) (result [2]int) {
 
-	var res [2]int = [2]int{0, 0}
+	var res [2]int = [2]int{-1, -1}
 
 	for i, v := range arr {
 
@@ -30,10 +30,15 @@ func main() {
 
 	arr := []int{222, 333, 11, 2, 3, 0, -2, 1, 2, 3}
 
-	var sum = 5
+	var sum = 0
 
 	result := twoSum(arr, sum)
 
-	fmt.Printf("arr[%d]:%d  + arr[%d]:%d  =  %d \n", result[0], arr[result[0]], result[1], arr[result[1]], sum)
+	if result[0] != -1 {
+
+		fmt.Printf("arr[%d]:%d  + arr[%d]:%d  =  %d \n", result[0], arr[result[0]], result[1], arr[result[1]], sum)
+	} else {
+		fmt.Println("cann't find the numbers")
+	}
 
 }
