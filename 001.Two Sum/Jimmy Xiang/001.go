13e85e7396ebdd197336a4831706592c117a7d5e
Jimmy Xiang <xxg1413@gmail.com>
2016-01-20 15:40:29 +0800

    update 001

--- 001.Two Sum/Jimmy Xiang/001.go
+++ 001.Two Sum/Jimmy Xiang/001.go
@@ -6,7 +6,7 @@ import (
 
 func twoSum(arr []int, value int) (result [2]int) {
 
-	var res [2]int = [2]int{-1, -1}
+	res := [2]int{-1, -1}
 
 	for i, v := range arr {
 
@@ -30,15 +30,14 @@ func main() {
 
 	arr := []int{222, 333, 11, 2, 3, 0, -2, 1, 2, 3}
 
-	var sum = 0
+	sum := 0
 
 	result := twoSum(arr, sum)
 
 	if result[0] != -1 {
 
 		fmt.Printf("arr[%d]:%d  + arr[%d]:%d  =  %d \n", result[0], arr[result[0]], result[1], arr[result[1]], sum)
-	} 
-	else {
+	} else {
 		fmt.Println("cann't find the numbers")
 	}
 
