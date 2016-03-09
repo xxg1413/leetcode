642cacf9826a36de3f750d59c20338b381d579ed
coderfive <pengwen.zeng@gmail.com>
2016-03-09 22:38:00 +0800

    solved some problems.

---
+++ 141.Linked List Cycle/coderfive/141.cc
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+	ListNode* slow = head, *fast = head;
+	while (fast && fast->next) {
+	    slow = slow->next;
+	    fast = fast->next->next;
+	    if (slow == fast) return true;
+	}
+	return false;
+    }
+};
