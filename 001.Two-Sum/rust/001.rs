4466839a34256520e3e0344120f1e61c57291768
Jimmy Xiang <xxg1413@gmail.com>
2019-09-25 23:52:54 +0800

    update 001

--- 001.Two-Sum/rust/001.rs
+++ 001.Two-Sum/rust/001.rs
@@ -12,3 +12,5 @@ impl Solution {
         vec![]
     }
 }
+
+
--- 001.Two-Sum/rust/two_sum/src/two_sum.rs
+++ 001.Two-Sum/rust/two_sum/src/two_sum.rs
@@ -3,12 +3,12 @@ use std::collections::HashMap;
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = HashMap::with_capacity(nums.len());
     for(index,num) in nums.iter().enumerate() {
-        if let Some(&find) = map.get(&(target-num)) {
-            return vec![find as i32, index as i32];
+        match map.get(&(target-num)) {
+            None => { map.insert(num,index); }
+            Some(find) => { return vec![*find as i32, index as i32]; }
         }
-        map.insert(num,index);
-     }
-     vec![]
+    }
+    vec![]
 }
 
 #[cfg(test)]
